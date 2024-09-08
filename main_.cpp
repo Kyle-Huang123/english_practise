fd2fa1cfeca74b663ec00e2f1c55be6f6f630ea9
Kyle-Huang123 <kyle970530@gmail.com>
2024-09-08 12:56:14 +0000

    1

--- main_.cpp
+++ main_.cpp
@@ -1,140 +1,69 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<string>
+
 using namespace std;
 
-int num1 = 0,num2 = 0,num3 = 0,num4 = 0,mode2 = 0;
-int correct_time = 0,incorrect_time = 0;
-bool running = true;
-vector<vector<string>> answer_list;
-string mode1 = "中翻英",answer = "a",again = "no"; 
-string english_answer, chinese_answer;
-string ans[] = {chinese_answer,english_answer}; 
-int mode(){
-    srand(time(NULL));
-    num3 = rand();
-    num3 = num3 % 2;
-    if(num3 == 0){
-        mode2 = 0;
-        return mode2;
-    }
-    else{
-        mode2 = 1;
-        return mode2;
-
-    }
-
+bool try_again(){
+    string again;
+    cout << "Do you want to try again? (Y/N): ";
+    cin >> again;
+    return (again == "Y");
 }
 
-bool try_again(){
-    cout << "Do you want to try again? (yes/no): ";
-    cin >> again;
-    return (again == "yes");
+void ask(vector<vector<string>> &ans_list,int mod = 0){
+    string q,a,answer;
+    int r = rand() % ans_list.size();
+    if(mod){
+        q = ans_list[r][0];
+        a = ans_list[r][1];
+    }
+    else{
+        q = ans_list[r][1];
+        a = ans_list[r][0];
+    }
+    ans_list.erase(ans_list.begin()+r);
+    cout<<"question: "<<q<<endl;
+    cout << "Your answer: ";
+    cin >> answer;
+    if(answer == a){
+        cout << "Correct!" << endl;
+    }
+    else{
+        cout << "Incorrect, the correct answer is: " << a << endl;
+    }
 }
 
 int main(){
+    srand(time(NULL));
+    vector<vector<string>> answer_list;
+    string english_answer, chinese_answer, mode;
+    int num1;
     
     cout << "請輸入有幾個單字";
     cin >> num1;
-    //answer_list.push_back(vector<string>(num1-1));
+
+    if(!(num1)) return 0;
     
     for(int i = 0;i < num1;i++){
         cout<<"請輸入第"<<i+1<<"個單字(英+空白+中)";
         cin >> english_answer >> chinese_answer;
         answer_list.push_back({english_answer,chinese_answer});
-        //answer_list[i].push_back(chinese_answer);
     }
-    
-    
-    /*cout << num1 << running << '\n';
-    cout << answer_list[0][1] << '\n';*/
+
     cout << "請選擇模式(A,B,C)(中翻英/英翻中/隨機)";
-    cin >> mode1;
-    srand(time(NULL));
-    if(mode1 == "A"||mode1 == "中翻英"){
-        while(num1 > 0 && running){
-            num2 = rand();
-            num2 = num2 % num1;
-            chinese_answer = answer_list[num2][1];
-            english_answer = answer_list[num2][0];
-            cout <<"題目是:" << chinese_answer << '\n';
-            cout << "答案:";
-            cin >> answer;
-           
-            if(answer == english_answer){
-                cout << "答對了!" << '\n';
-                correct_time ++;
-            }
-            else{
-                cout << "答錯了" << '\n';
-                incorrect_time ++;
+    cin >> mode;
 
-            }
-            answer_list.erase(answer_list.begin() + num1);
-            num1 -= 1;
-           
-            if(!(num1 > 0&&try_again())){
-                return 0;
-            }
+    do{
+        if(mode == "A"||mode == "中翻英"){
+            ask(answer_list);
         }
-    }
-    else if(mode1 == "英翻中"||mode1 == "B"){
-        while(num1 > 0 && running){
-            num2 = rand();
-            num2 = num2 % num1;
-            english_answer = answer_list[num2][0];
-            chinese_answer = answer_list[num2][1];
-            cout << "題目是:" << english_answer << '\n';
-            cout << "答案:";
-            cin >> answer;
-           
-            if(answer == chinese_answer){
-                cout << "答對了!" << '\n';
-                correct_time ++;
-
-            }
-            else{
-                cout << "答錯了!" << '\n';
-                incorrect_time ++;
-
-            }
-            answer_list.erase(answer_list.begin() + num1);
-            num1 -= 1;
-           
-            if(!(num1 > 1&&try_again())){
-                return 0;
-            }
-
+        else if(mode == "B"||mode == "英翻中"){
+            ask(answer_list,1);
         }
-            
-    }
-    else if(mode1 == "隨機"||mode1 == "C"){
-        while(running && num1 > 1){
-            if(mode2 == 0){
-                num4 = 0;
-            }
-            else if(mode2 == 1){
-                num4 = 1;
-            }
-            num2 = rand();
-            num2 = num2 % num1;
-            cout << "題目是:" << answer_list[num2][num4] << '\n';
-            cout << "答案:";
-            cin >> answer;
-            if(answer == answer_list[num2][1-num4]){
-                cout << "答對了!" << '\n';
-                correct_time ++;
-            }
-            else{
-                cout << "答錯了!" << '\n';
-                incorrect_time ++;
-
-            }
-            if(!(num1 > 1&&try_again())){
-                return 0;
-            }
-            
-
+        else if(mode == "C"||mode == "隨機"){
+            int randmode = rand() % 2;
+            ask(answer_list,randmode);
         }
-        
-    }
-    return 0;
+    }while(answer_list.size()&&!(try_again()));
 }
