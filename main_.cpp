e22489530eee47038d3175a1e11561f097cd922e
Kyle-Huang123 <kyle970530@gmail.com>
2024-09-08 07:58:55 +0000

    1

--- main_.cpp
+++ main_.cpp
@@ -30,26 +30,36 @@ int main(){
     cout << "請輸入有幾個單字";
     cin >> num1;
     answer_list.push_back(vector<string>(num1-1));
-    for(int i = 0;i < num1;i++)
-    {
-        for(int j = 0;j <= 2;j++){
-            printf("請輸入第%d個單字以及單字的中文意思(請用一個空白鍵隔開)",i+1);
-            cin >> english_answer >> chinese_answer;
-            answer_list[i].push_back(english_answer);
-            answer_list[i].push_back(chinese_answer);
-        }
+    if(num1 == 1){
+        printf("請輸入第1個單字以及單字的中文意思(請用一個空白鍵隔開)");
+        cin >> english_answer >> chinese_answer;
+        answer_list[0].push_back(english_answer);
+        answer_list[0].push_back(chinese_answer);
+    }
+    else{
+        for(int i = 0;i < num1;i++)
+        {
+            for(int j = 0;j < 2;j++){
+                printf("請輸入第%d個單字以及單字的中文意思(請用一個空白鍵隔開)",i+1);
+                cin >> english_answer >> chinese_answer;
+                answer_list[i].push_back(english_answer);
+                answer_list[i].push_back(chinese_answer);
+            }
     
+        }
     }
     
+    /*cout << num1 << running << '\n';
+    cout << answer_list[0][1] << '\n';*/
     cout << "請選擇模式(中翻英/英翻中/隨機)";
     cin >> mode1;
     srand(time(NULL));
     if(mode1 == "中翻英"){
-        while(num1 > 0 & running){
+        while(num1 > 0 && running){
             num2 = rand();
             num2 = num2 % num1;
-            chinese_answer = answer_list[1][num2];
-            english_answer = answer_list[0][num2];
+            chinese_answer = answer_list[num2][1];
+            english_answer = answer_list[num2][0];
             cout <<"題目是:" << chinese_answer << '\n';
             cout << "答案:";
             cin >> answer;
@@ -83,7 +93,7 @@ int main(){
         }
     }
     else if(mode1 == "英翻中"){
-        while(num1 > 0 & running){
+        while(num1 > 0 && running){
             num2 = rand();
             num2 = num2 % num1;
             english_answer = answer_list[num2][0];
@@ -127,7 +137,7 @@ int main(){
             
     }
     else if(mode1 == "隨機"){
-        while(running & num1 > 1){
+        while(running && num1 > 1){
             if(mode2 == 0){
                 num4 = 0;
             }
